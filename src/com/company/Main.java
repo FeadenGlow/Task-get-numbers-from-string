93d1bcd7fa22dde16b15104414611dba96fb5f32
Minezor <minezor.github.com>
2020-04-11 15:22:20 +0300

    Add the template

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
